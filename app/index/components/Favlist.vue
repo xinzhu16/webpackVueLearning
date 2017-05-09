a118c6abc83e42cec4f7e53b9bd958b0745f4ec6
xinzhu16 <xinzhu16@163.com>
2017-05-09 21:12:07 +0800

    modify

--- app/index/components/Favlist.vue
+++ app/index/components/Favlist.vue
@@ -2,6 +2,12 @@
     <div>
         <div v-for="n in 10">{{msg}}</div>
         <a href="./index.html">test</a>
+        <div>
+            <div class = 'd1'>dddddddddddddddddddddd1</div>
+            <div class="d2">llllllllllllllllllllllllllllllllllllll</div>
+            <div class="d1">dddddddddddddddddddd</div>
+            <div class="d2">mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm</div>
+        </div>
     </div>
 </template>
 
@@ -17,6 +23,14 @@
 
 <style>
     html{
-        background: blue;
+        /*background: blue;*/
+    }
+    .d2{
+        position: relative;
+        background: red;
+    }
+    .d1{
+        position: absolute;
+        color:yellow;
     }
 </style>
