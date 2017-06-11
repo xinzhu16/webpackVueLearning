26c14c30f858ba9dd14b370fd6ddb303739bff8f
xinzhu16 <xinzhu16@163.com>
2017-06-12 00:03:21 +0800

    使用了懒加载，不过貌似没有变化，得去查看懒加载的原理

--- app/index/components/router.js
+++ app/index/components/router.js
@@ -2,11 +2,21 @@
 /**
  * Created by Administrator on 17/6/11.
  */
-import flex from './flex';
-import boxModel from './boxModel';
-import Favlist from './Favlist';
+// import flex from './flex';
+// import boxModel from './boxModel';
+// import Favlist from './Favlist';
+// import centerAlign from './centerAlign';
+// import debounce from './debounce';
+// import nextTick from 'nextTick';
+// import nthChild from 'nthChild';
+// import prototype from ''
+var arrRouter = ['boxModel','centerAlign','debounce','Favlist','flex','nextTick',
+    'nthChild','prototype', 'slot','temp','transition'];
+var routers = [{'path':'',redirect:'Favlist'}];
+routers.push.apply(routers,arrRouter.map(function(item)
+{return {'path':item,component:resolve=>require(['./'+item],resolve)}}));
+console.log('ddddddddddddddddd')
+console.log(routers);
 export default {
-    router:[{'path':'',redirect:'box'},
-        {'path':'flex',component:Favlist},
-        {path:'box',component:boxModel}]
+    router:routers
 }
